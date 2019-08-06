f1c7b50d524742af2d5a9e823c98ba9c12a7668d
Daria Velichko <danyvelik222@gmail.com>
2019-08-06 23:41:13 +0300

    added task1
    added empty  task2

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("s= 25 ");
+            float s = float.Parse(Console.ReadLine());
+            double p = 4 * Math.Sqrt(s);
+            Console.WriteLine("p=" + p);
+        }
+    }
+}
